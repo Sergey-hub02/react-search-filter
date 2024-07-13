97d3fc15964c4a9fe3d688748fd720d7c6291d52
Sergey-hub02 <s.park190802@gmail.com>
2024-07-13 17:20:16 +0300

    Реализовал логику фильтрации

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { ChangeEvent } from 'react';
+import { names } from './names';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    let [namesList, setNamesList] = useState(names);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    const handleChange = (event: ChangeEvent) => {
+        const value = (event.target as HTMLInputElement).value;
 
-export default App
+        if (value.length === 0) {
+            setNamesList(names);
+            return;
+        }
+
+        const newList = names.filter(name => name
+            .toLowerCase()
+            .includes(value)
+        );
+
+        setNamesList(newList);
+    };
+
+    return (
+        <div className="App">
+            <form action="">
+                <div className="field-box">
+                    <input
+                        id="name"
+                        type="text"
+                        name="name"
+                        className="field"
+                        placeholder="Type a name..."
+                        onChange={handleChange}
+                    />
+                </div>
+            </form>
+
+            <div className="names-list">
+                {namesList.map((name, index) => {
+                    return <div key={index}>{name}</div>;
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/names.ts
@@ -0,0 +1,7 @@
+export const names = [
+    'Pak Sergey',
+    'Khakimov Renat',
+    'Milko Maksim',
+    'Glushko Nikita',
+    'Zhinznevskiy Nikita',
+];
